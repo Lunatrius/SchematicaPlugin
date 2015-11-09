c0a9fa959180680380cabce3d594a864d34d7d6f
Lunatrius <jadran.kotnik@gmail.com>
2015-11-09 21:45:07 +0100

    Replace config file with permission nodes.

--- src/main/java/com/github/lunatrius/schematica/plugin/SchematicaPlugin.java
+++ src/main/java/com/github/lunatrius/schematica/plugin/SchematicaPlugin.java
@@ -1,68 +1,30 @@
 package com.github.lunatrius.schematica.plugin;
 
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
-import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
 
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 
 public class SchematicaPlugin extends Plugin implements Listener {
-    private static final String CONFIG_FILE_NAME = "config.yml";
-    private File configFile = null;
-    private boolean isPrinterEnabled = true;
-    private boolean isSaveEnabled = true;
-    private boolean isLoadEnabled = true;
+    private static final String PERM_PRINTER = "schematica.printer";
+    private static final String PERM_SAVE = "schematica.save";
+    private static final String PERM_LOAD = "schematica.load";
 
     @Override
     public void onLoad() {
-        this.configFile = new File(getDataFolder(), CONFIG_FILE_NAME);
     }
 
     @Override
     public void onEnable() {
-        copyDefaultConfiguration();
-        loadConfiguration();
-
         getProxy().getPluginManager().registerListener(this, this);
         getProxy().registerChannel("schematica");
     }
 
-
-    private void copyDefaultConfiguration() {
-        final File dataFolder = this.configFile.getParentFile();
-        if (!dataFolder.exists()) {
-            dataFolder.mkdirs();
-        }
-
-        if (!this.configFile.exists()) {
-            try (final InputStream in = getResourceAsStream(CONFIG_FILE_NAME)) {
-                Files.copy(in, this.configFile.toPath());
-            } catch (final IOException ioe) {
-                getLogger().throwing(SchematicaPlugin.class.getName(), "copyDefaultConfig", ioe);
-            }
-        }
-    }
-
-    private void loadConfiguration() {
-        try {
-            final Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(this.configFile);
-
-            this.isPrinterEnabled = configuration.getBoolean("capability.printer", true);
-            this.isSaveEnabled = configuration.getBoolean("capability.save", true);
-            this.isLoadEnabled = configuration.getBoolean("capability.load", true);
-        } catch (final IOException ioe) {
-            getLogger().throwing(SchematicaPlugin.class.getName(), "loadConfiguration", ioe);
-        }
-    }
 
     @Override
     public void onDisable() {
@@ -72,17 +34,21 @@ public class SchematicaPlugin extends Plugin implements Listener {
 
     @EventHandler
     public void onPostLogin(final PostLoginEvent event) {
-        event.getPlayer().sendData("schematica", getPayload());
+        final ProxiedPlayer player = event.getPlayer();
+        final byte[] payload = getPayload(player);
+        if (payload != null) {
+            player.sendData("schematica", payload);
+        }
     }
 
-    private byte[] getPayload() {
+    private byte[] getPayload(ProxiedPlayer player) {
         final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         final DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
         try {
             dataOutputStream.writeByte(0);
-            dataOutputStream.writeBoolean(this.isPrinterEnabled);
-            dataOutputStream.writeBoolean(this.isSaveEnabled);
-            dataOutputStream.writeBoolean(this.isLoadEnabled);
+            dataOutputStream.writeBoolean(player.hasPermission(PERM_PRINTER));
+            dataOutputStream.writeBoolean(player.hasPermission(PERM_SAVE));
+            dataOutputStream.writeBoolean(player.hasPermission(PERM_LOAD));
 
             return byteArrayOutputStream.toByteArray();
         } catch (final IOException ioe) {
